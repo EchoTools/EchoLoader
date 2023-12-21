cd85ac1bd8950a3b90d9a850c5f285d49da3fe93
Christian Luddy <Cluddy05@gmail.com>
2023-12-20 19:18:13 -0500

    Remove console/logs

--- EchoLoader/dllmain.cpp
+++ EchoLoader/dllmain.cpp
@@ -4,48 +4,35 @@
 #include <filesystem>
 
 VOID MAIN() {
-    // Create console for logging
-    AllocConsole();
-    FILE* f;
-    freopen_s(&f, "CONOUT$", "w", stdout);
-    std::cout << "[EML] Initializing" << std::endl;
-    // Find "dlls" folder within directory 
-    std::string DllFolder = std::filesystem::current_path().string() + "\\dlls";
+	// Find "dlls" folder within directory 
+	std::filesystem::path DllFolder = std::filesystem::current_path() / "dlls";
 
-    // Make sure "dlls" folder exists
-    if (!std::filesystem::exists(DllFolder)) {
-		std::cout << "[EML] Failed to find dlls folder" << std::endl;
-        // Create dlls folder
-        std::filesystem::create_directory(DllFolder);
-        std::cout << "[EML] Created dlls folder" << std::endl;
+	// Make sure "dlls" folder exists
+	if (!std::filesystem::exists(DllFolder)) {
+		// Create dlls folder
+		std::filesystem::create_directory(DllFolder);
 	}
 
-    std::cout << "[EML] Looking for DLLs in " << DllFolder << std::endl;
-    // Iterate through all files in "dlls" folder
-    for (const auto& entry : std::filesystem::directory_iterator(DllFolder)) {
+	// Iterate through all files in "dlls" folder
+	for (const auto& entry : std::filesystem::directory_iterator(DllFolder)) {
 		std::string DllPath = entry.path().string();
-        // Make sure it's a DLL
-        if (DllPath.find(".dll") == std::string::npos) continue;
 
-        // Load DLL
+		// Make sure it's a DLL
+		if (DllPath.find(".dll") == std::string::npos) continue;
+
+		// Load DLL
 		HMODULE Dll = LoadLibraryA(DllPath.c_str());
-        if (Dll == NULL) {
-			std::cout << "[EML] Failed to load " << DllPath << std::endl;
-		}
-        else {
-			std::cout << "[EML] Loaded " << DllPath << std::endl;
-		}
 	}
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
 		MAIN();
 	}
-    return TRUE;
+	return TRUE;
 }
 
